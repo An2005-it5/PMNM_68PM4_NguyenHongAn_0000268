ea809eb50fdf93be231215bdf4a39bbc548b2473
[200~An2005-it5~ <nguyenhongan17t@gmail.com>
2026-05-15 16:04:57 +0700

    Initial commit: MVC PHP QLSV

---
+++ app/controllers/sinhvien.php
@@ -0,0 +1,9 @@
+<?php
+class sinhvien {
+    public function index() {
+        echo 'Đây là Danh sách sinh viên';
+    }
+    public function create() {
+        echo 'Đây là trang tạo mới sinh viên';
+    }
+}
